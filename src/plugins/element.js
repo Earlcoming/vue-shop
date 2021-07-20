ecb03d99d4d1a5a11effed20336ece0d5bc15c4d
Earlcoming <earlcoming@163.com>
2021-07-20 16:01:37 +0800

    'login'

---
+++ src/plugins/element.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import {
+	Button,
+	Input,
+	Form,
+	FormItem,
+} from 'element-ui'
+
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Form);
+Vue.use(FormItem);
---
+++ src/views/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="login_container">
+        <div class="login_text">
+            <el-form :model="loginForm">
+                <el-form-item>
+                    <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+                </el-form-item>
+				<el-form-item>
+                    <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
+                </el-form-item>
+				 <el-form-item>
+					<el-button type="primary" @click="login()">登录</el-button>
+					<el-button @click="reset()">重置</el-button>
+				</el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data() {
+        return {
+            loginForm: {
+                username: "admin",
+                password: 123456,
+            },
+			rules: {
+				password: []
+			}
+        };
+    },
+    components: {},
+	methods: {
+		reset() {
+
+		},
+		login() {
+
+		}
+	}
+};
+</script>
+
+<style lang='sass' scoped>
+</style>
