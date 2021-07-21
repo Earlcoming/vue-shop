a9f2512c06b7e38e26b4f1245a918724e1d54280
Earlcoming <earlcoming@163.com>
2021-07-21 17:36:16 +0800

    'home'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,10 +6,26 @@ import {
 	FormItem,
 	Input,
 	Message,
+	Header,
+	Aside,
+	Container,
+	Main,
+	Menu,
+	Submenu,
+	MenuItem,
+	MenuItemGroup,
 } from 'element-ui'
 
 Vue.use(Button);
 Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Container);
+Vue.use(Menu);
+Vue.use(Main);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
 Vue.prototype.$msg = Message;
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,20 +1,72 @@
 <template>
-  <div class="home">
-	  <el-button type="info" @click="logout">退出登录</el-button>
-  </div>
+    <el-container class="home-container">
+        <!-- 头部 -->
+        <el-header class="home-header">
+            <div class="logo">电商后台管理</div>
+            <el-button type="info" @click="logout">退出登录</el-button>
+        </el-header>
+        <el-container>
+            <!-- 侧边 -->
+            <el-aside width="200px">
+                <el-col :span="12">
+                    <h5>自定义颜色</h5>
+                    <el-menu
+                        class="el-menu-vertical-demo"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        background-color="#333744"
+                        text-color="#fff"
+                        active-text-color="#409eff"
+                    >
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+							<el-menu-item index="1-1">选项1</el-menu-item>
+							<el-menu-item index="1-2">选项2</el-menu-item>
+                        </el-submenu>
+						
+                    </el-menu>
+                </el-col>
+            </el-aside>
+            <!-- 主体 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
-	methods: {
-		logout () {
-			window.sessionStorage.clear('token');
-			this.$router.push({name: 'login'})
-		}
-	}
-}
+    methods: {
+        logout() {
+            window.sessionStorage.clear("token");
+            this.$router.push({ name: "login" });
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+};
 </script>
 
-<style>
-
+<style lang="sass">
+.home-container
+	height: 100%
+	.el-header
+		background-color: #373d41
+		color: #fff
+		&.home-header
+			display: flex
+			justify-content: space-between
+			align-items: center
+			padding: 0 15px
+	.el-aside
+		background-color: #333744
+		color: #fff
+	.el-main
+		background-color: #eaedf1
 </style>
