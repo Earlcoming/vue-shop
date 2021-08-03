d2aaa5bac6a6341a58c32f4f199ec0125d7467a6
Earl <earlcoming@163.com>
2021-08-04 00:46:53 +0800

    'production'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,38 @@
+
 module.exports = {
-	devServer: {
-		hot: true
+	chainWebpack: config => {
+		// 发布模式
+		config.when(process.env.NODE_ENV === 'production', config => {
+			config.entry('app').clear().add('./src/main-prod.js');
+
+			// 设置过滤包的名,不打包这些包,在public/index.html中加入cdn
+			config.set('externals', {
+				vue: 'vue',
+				axios: 'axios',
+				'vue-quill-editor': 'VueQuillEditor',
+				nprogress: 'NProgress',
+				echarts: 'echarts',
+				'vue-router': 'VueRouter'
+			})
+
+			// 给html属性添加isProd属性为true;
+			config.plugin('html').tap(args => {
+				args[0].isProd = true;
+				return args;
+			});
+			
+		})
+
+		// 开发模式
+		config.when(process.env.NODE_ENV === 'development', config => {
+			config.entry('app').clear().add('./src/main-dev.js');
+			// 给html属性添加isProd为false
+			config.plugin('html').tap(args => {
+				args[0].isProd = false;
+				return args;
+			});
+		
+			
+		})
 	}
 }
